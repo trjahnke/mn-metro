8479bae37214b0677390b8448ad86a4a1ce8378b
Tristan <trjahnke@gmail.com>
2020-02-20 20:46:24 -0600

    rename

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="mn_metro-trjahnke", # Replace with your own username
+    name="mn-metro", # Replace with your own username
     version="0.0.1",
     author="Tristan Jahnke",
     author_email="trjahnke@gmail.com",
     description="A wrapper for the Minnesota Metro API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/trjahnke/mn_metro",
+    url="https://github.com/trjahnke/mn-metro",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
