19c352eef3c5f81078769089e0a6da3637420f6d
trjahnke <trjahnke@gmail.com>
2020-02-20 20:27:38 -0600

    setup add

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mn_metro-trjahnke", # Replace with your own username
+    version="0.0.1",
+    author="Tristan Jahnke",
+    author_email="trjahnke@gmail.com",
+    description="A wrapper for the Minnesota Metro API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/trjahnke/mn_metro",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
